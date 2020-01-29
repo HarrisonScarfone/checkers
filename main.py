0ef55fbe917cb5221d490f89b8ecbb1af4b5d8be
HarrisonScarfone <hscarfone77@gmail.com>
2020-01-28 20:11:40 -0500

    added ability to find empty spaces for checker to move to. added iteration through all pieces to get all moves.

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from board.board import Board
 b = Board()
 b.draw_board()
 
-print(b.get_piece_from_location(4,1))
-#
-# b.get_all_possible_moves()
+print(b.get_all_white_moves())
+
+print(b.get_all_black_moves())
